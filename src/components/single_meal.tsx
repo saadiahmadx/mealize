804a60646c4afd856d221ecc0db79f2ae670fde0
nick-casa <nc16@princeton.edu>
2022-10-31 16:28:39 -0400

    FINISHED PROTOTYPE

--- src/components/single_meal.tsx
+++ src/components/single_meal.tsx
@@ -1,10 +1,11 @@
-export interface FoodItem{
-    name: string;
-    quantity: number;
-}
+
 export interface Meal{
-    day: string
-    components: FoodItem[]
+    label: string
+    components: string[]
+}
+export interface Day{
+    label: string
+    components: Meal[]
 }
 
 const refreshIcon = <svg className="h-4 w-4 m-2"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -5v5h5" />  <path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 5v-5h-5" /></svg>
@@ -14,20 +15,28 @@ export const SimpleMeal = (props:Meal) => {
         <div className="max-w-sm min-w-fit rounded overflow-hidden shadow-lg">
             <div className="flex flex-row justify-between bg-[#159539] text-white p-3">
                 <>
-                <p className="m-2">{props.day}</p>{refreshIcon}
+                <p className="m-1">{props.label}</p>
                 </>
             </div>
             {
             props.components.map((food_item)=>{
                 return (
-                <div className="w-60 px-5 py-4 flex flex-row justify-between">
-                    <div className="text-left">{food_item.name}</div>
-                    <div className="text-right">{food_item.quantity+"g"}</div>
+                <div className="w-40 px-4 py-2 flex flex-row justify-between">
+                    <div className="text-left">{food_item}</div>
                 </div>
                 );
             })
             }
             <hr/>
+        </div>
+    </>);
+}
+
+export const SimpleDay = (props:Day) => {
+    return(<>
+        <div className="p-5 flex flex-col justify-between gap-5 overflow-hidden ">
+            <h3><strong>{props.label}</strong></h3>
+            {props.components.map((meal) => {return SimpleMeal(meal)})}
         </div>
     </>);
 }
--- src/pages/meals.tsx
+++ src/pages/meals.tsx
@@ -1,24 +1,54 @@
-import { SimpleMeal, Meal, FoodItem } from "../components/single_meal"
+import { SimpleDay, Meal, FoodItem, Day } from "../components/single_meal"
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 
 export default function Meals(){
-    let foods:FoodItem[] = [{name:"Broccoli", quantity: 2},{name:"Cheese", quantity: 10},{name:"Chicken", quantity: 20}];
-    let meal:Meal = {day: "Tuesday (10/19)", components: foods}
+    const [days, setDays] = useState<Day[]>();
 
+    const getMealPlan = async () => {
+        console.log("Geenerating meal plan...")
+        let response = await axios.get("http://ec2-44-203-126-101.compute-1.amazonaws.com:5000/",{params:{protein:50, carbs: 50, fats: 50}})
+        return response.data
+    }
+
+    const updateWeek = async () => {
+        getMealPlan().then((res)=>{
+            let newDays:Day[] = [];
+            res.forEach((day:any)=>{
+                let breakfast:Meal = {label: "Breakfast", components: day.breakfast}
+                let lunch:Meal = {label: "Lunch", components: day.lunch}
+                let dinner:Meal = {label: "Dinner", components: day.dinner}
+                let dayOfWeek:Day = {label: "Test", components: [breakfast,lunch,dinner]}
+                newDays.push(dayOfWeek)
+            })
+            setDays(newDays)
+        })
+    }
+
+    useEffect(()=>{
+        updateWeek()
+    },[])
+
+    useEffect(()=>{console.log(days)},[days])
 
     return(
         <div className="flex flex-col">
 
-            <div className="p-5 flex flex-row justify-between gap-5 overflow-hidden ">
-                {SimpleMeal(meal)}
-                {SimpleMeal(meal)}
-                {SimpleMeal(meal)}
+            <div className="p-5 flex flex-row justify-between gap-1 overflow-hidden ">
+                {days?
+                <>
+                {days.map((day)=>{
+                    return SimpleDay(day)
+                })}
+                </>
+                :'Loading...'}
             </div>
 
             <br/>
 
-            <button className="text-white bg-[#159539] font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-                        Generate new week
+            <button onClick={updateWeek} className="text-white bg-[#159539] font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                Generate new week
             </button>
 
         </div>
